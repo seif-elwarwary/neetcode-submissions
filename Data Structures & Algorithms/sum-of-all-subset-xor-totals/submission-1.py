9a0bf470e12c4ebac3908e34408c7275b81fcd72
Seif Elwarwary <seifelwarwary@gmail.com>
2026-08-09 00:52:44 +0300

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.py
@@ -0,0 +1,19 @@
+from functools import reduce
+import operator
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        total = 0
+        arr = []
+        def dfs(i):
+            nonlocal total
+            if i == len(nums): 
+                total+= reduce(operator.xor, arr,0)
+                return
+            arr.append(nums[i])
+            dfs(i+1)
+            arr.pop()
+            dfs(i+1)
+
+        dfs(0)
+        return total
