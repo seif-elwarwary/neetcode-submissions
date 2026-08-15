9cf05aa1c70a0ef9b427c2b8fd9d36fc606fc26e
Seif Elwarwary <seifelwarwary@gmail.com>
2026-08-15 23:56:09 +0300

    Add: combination-sum-iv - submission-2

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        cache = {0:1}
+        def dfs(total):
+            if total in cache: return cache[total]
+            res=0
+            for i in range(len(nums)):
+                val = total-nums[i]
+                if val < 0 : break
+                res+=dfs(val)
+            cache[total] = res
+            return res
+        return dfs(target)
